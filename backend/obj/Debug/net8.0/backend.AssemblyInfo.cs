04175989a2d1c4b6d400ed81cf53e4e39c6eaedc
Kamilek <radek71k@onet.pl>
2024-07-16 14:13:30 +0200

    Update and Delete endpoints

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e03634b58cda730d688e2f092154f051f65d7b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a622cd0d5ccc5963b3169a557da8050afe49f98e")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
