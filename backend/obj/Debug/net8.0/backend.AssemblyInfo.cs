be9184f305307c9a2fee5282ec5446538e561bce
Kamilek <radek71k@onet.pl>
2024-07-16 14:23:31 +0200

    Making endpoints async

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a622cd0d5ccc5963b3169a557da8050afe49f98e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04175989a2d1c4b6d400ed81cf53e4e39c6eaedc")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
