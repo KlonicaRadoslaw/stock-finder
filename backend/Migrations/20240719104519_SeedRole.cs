ba9f11147b2ffaed8d9b7c6e2bc3e867f10e094c
Kamilek <radek71k@onet.pl>
2024-07-19 13:06:02 +0200

    Registration endpoint

---
+++ backend/Migrations/20240719104519_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4ccfc6f3-a0b9-4d49-8bee-0468b8c816ba", null, "User", "USER" },
+                    { "8a83605a-c49e-4706-a84d-7bfd678a2010", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4ccfc6f3-a0b9-4d49-8bee-0468b8c816ba");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8a83605a-c49e-4706-a84d-7bfd678a2010");
+        }
+    }
+}
