6d0d3dbdedfeace2bb04a15dcf0d393064e8e062
Kamilek <radek71k@onet.pl>
2024-07-20 12:28:18 +0200

    Adding stock to user's portfolio

--- backend/Interfaces/IPortfolioRepository.cs
+++ backend/Interfaces/IPortfolioRepository.cs
@@ -5,5 +5,6 @@ namespace backend.Interfaces
     public interface IPortfolioRepository
     {
         Task<List<Stock>> GetUserPortfolio(AppUser user);
+        Task<Portfolio> CreateAsync(Portfolio portfolio);
     }
 }
