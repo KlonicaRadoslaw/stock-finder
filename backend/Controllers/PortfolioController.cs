d873a33dcf3931dde78bf028f94ec218cac485ac
Kamilek <radek71k@onet.pl>
2024-07-20 11:57:23 +0200

    GET user's portfolio

---
+++ backend/Controllers/PortfolioController.cs
@@ -0,0 +1,37 @@
+﻿using backend.Extensions;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepository;
+        private readonly IPortfolioRepository _portfolioRepository;
+        public PortfolioController(UserManager<AppUser> userManager, 
+            IStockRepository stockRepository,
+            IPortfolioRepository portfolioRepository)
+        {
+            _stockRepository = stockRepository;
+            _userManager = userManager;
+            _portfolioRepository = portfolioRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+
+            return Ok(userPortfolio);
+        }
+    }
+}
